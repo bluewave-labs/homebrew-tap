28914707cabb1e06d2ab9d23571b07100042daa9
gorkem-bwl <gorkem.cetin@bluewavelabs.ca>
2026-01-28 23:42:47 -0500

    fix: Update SHA256 for fancy DMG

--- Casks/dodoclip.rb
+++ Casks/dodoclip.rb
@@ -1,6 +1,6 @@
 cask "dodoclip" do
   version "1.2.0"
-  sha256 "8652d578135b72449e43ec47df33ed45b2e78827597a91ef4de85a3e971ac0df"
+  sha256 "9c01572e177eb27ba6a1e920e43be50052261a803f6bea9155ae6df68a167e89"
 
   url "https://github.com/bluewave-labs/dodoclip/releases/download/v#{version}/DodoClip-#{version}.dmg"
   name "DodoClip"
